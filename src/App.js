6400b11a9b0dedcca0c9bc98e4024e73458afc1a
Rui Afonso Pereira <ruiafonsopereira@gmail.com>
2016-10-30 16:51:16 +0000

    Render list of persons

--- src/App.js
+++ src/App.js
@@ -2,8 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const personsList = [
+  {
+    name: 'Catarina Costa',
+    age: 23
+  },
+  {
+    name: 'Rui Pereira',
+    age: 24
+  },
+  {
+    name: 'Maria Antonia',
+    age: 54
+  },
+  {
+    name: 'Manel das Dores',
+    age: 78
+  },
+];
+
 class App extends Component {
-  render() {
+  state = {
+    persons: personsList,
+  }
+
     return (
       <div className="App">
         <div className="App-header">
@@ -13,6 +35,20 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+  renderPerson(person, id) {
+    return (
+      <li key={id} className="list-item">
+        <div>{`Name: ${person.name}`}</div>
+        <div>{`Age: ${person.age}`}</div>
+      </li>
+    );
+  }
+
+  render() {
+    return (
+        <ul>
+          {this.state.persons.map((person, index) => this.renderPerson(person, index))}
+        </ul>
       </div>
     );
   }
